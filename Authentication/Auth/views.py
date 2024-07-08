948340b196394b7e8492eda7b434235738ab39b5
Aliwangara <aliwangara63@gmail.com>
2024-07-08 18:04:38 +0300

    Changed views post
    Changed views post name to Capital letters

--- Authentication/Auth/views.py
+++ Authentication/Auth/views.py
@@ -7,12 +7,12 @@ from django.contrib.auth.models import User
 # Create your views here.
 def sign_up(request):
     if request.method == "POST":
-        f_name = request.post.get('FirstName')
-        s_name = request.post.get('LastName')
-        mail = request.post.get('email')
-        Dob = request.post.get('date')
-        first_password = request.post.get('pass1')
-        second_password = request.post.get('pass2')
+        f_name = request.POST.get('FirstName')
+        s_name = request.POST.get('LastName')
+        mail = request.POST.get('email')
+        Dob = request.POST.get('date')
+        first_password = request.POST.get('pass1')
+        second_password = request.POST.get('pass2')
 
         if first_password != second_password:
             messages.info(request, "password not matching")
@@ -27,6 +27,6 @@ def sign_up(request):
         myuser = User.objects.create_user(mail, mail, first_password)
         myuser.save
         messages.success(request, "User Created Successfully")
-        return redirect('signin')
+        # return redirect('signin')
 
     return render(request, "Signup.html")
