c3d50807ea594a076492b807e86c6e4ab0f20219
Aliwangara <aliwangara63@gmail.com>
2024-07-08 17:44:14 +0300

    created signup views
    started Creating signup views using inbuilt django authentication system

--- Authentication/Auth/views.py
+++ Authentication/Auth/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
+from  django.contrib.auth import authenticate, login,logout
+from django.contrib.auth.models import User
 
 # Create your views here.
 def sign_up(request):
-    pass
+    if request.method =="POST":
+        f_name = request.post.get('FirstName')
+        s_name = request.post.get('LastName')
+        mail = request.post.get('email')
+        Dob = request.post.get('date')
+        first_password = request.post.get('pass1')
+        second_password = request.post.get('pass2')
+        
+        if first_password != second_password:
+            message.info(request, "password not matching")
+            
+        
+
+    
     return render(request, "Signup.html")
