679194d93672975a39401f32d43cb42126bf995c
Aliwangara <aliwangara63@gmail.com>
2024-07-08 21:40:58 +0300

    Update urls.py
    added home url

--- Authentication/Auth/urls.py
+++ Authentication/Auth/urls.py
@@ -3,5 +3,7 @@ from django.urls import path,include
 from. import views
 
 urlpatterns = [
-    path('sign_up', views.sign_up, name='sign_up' )
+    path('home', views.home, name='home'),
+    path('sign_up', views.sign_up, name='sign_up'),
+     path('Log', views.Log, name='Log'),
 ]
